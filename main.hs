63f5f7fc19be4f3ec00cbf87506884a6f5c6ec10
Salmon Thomas <th_s@hotmail.fr>
2013-07-31 01:56:57 +0200

    push

--- main.hs
+++ main.hs
@@ -1,7 +1,17 @@
-{-
-	CALLBY
+{- 
+2  []
+3  []
+4  [2]
+5  []
+6  [2, 3]
+7  []
+8  [2, 4]
+9  [3]
+10 [2, 5]
+
 -}
 
-isIn l k = case l of 
-	[] -> False
-	a:b -> a==k || isIn b k
+f x = [ b |  a <- [1..10], b <- [2..(a-1)], a `mod` b / 0]
+
+main = do 
+  print (f 4)
