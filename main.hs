b059bff14442f01bd0372079c1952e5e95c26ce3
Salmon Thomas <th_s@hotmail.fr>
2013-08-01 00:29:27 +0200

    add algorithm description

--- main.hs
+++ main.hs
@@ -1,19 +1,31 @@
 import System.Environment
-import System.IO
-
+import System.IO  
+{- 
+1)read the file given in parameters
+2)str is function declaration? -> add his name in fctn_list, enter in it [[name]...]
+3)for all call of functions, add their names add after the name of the function [[name, c1, .., cn]]
+4)at the end of file, read again the file
+5)if str is a function declaration:
+  call = "" 
+  for all elems in fctn_list as elem:
+     for all elems in elem[1..]: 
+        if elems == str then
+           call += elem[0]
+  insert file_text call (position str -1)
+6)write file_text in the file
+-}
+  
 fctn_list = []
-
-
-
--- insert txt into str at position p
+            
+-- insert txt into str at position p 
 insert str txt p = 
   if(p < (length str)) then 
     let a = (take p . drop 0) str in
     let b = (take (length str) . drop p) str in
     a ++ txt ++ b
   else
-    error " > str"
-
+    error "out of length"
+  
 algo x = print x
 
 readfile f = do
