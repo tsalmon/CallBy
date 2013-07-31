7fb03185d86728cc343a0efec2d32795989c3365
Salmon Thomas <th_s@hotmail.fr>
2013-07-31 23:28:15 +0200

    add insert method

--- main.hs
+++ main.hs
@@ -1,10 +1,24 @@
 import System.Environment
 import System.IO
 
+fctn_list = []
+
+
+
+-- insert txt into str at position p
+insert str txt p = 
+  if(p < (length str)) then 
+    let a = (take p . drop 0) str in
+    let b = (take (length str) . drop p) str in
+    a ++ txt ++ b
+  else
+    error " > str"
+
+algo x = print x
+
 readfile f = do
   outh <- openFile f ReadMode
   x <- hGetContents outh
-  putStrLn x
   hClose outh
 
 main = do
